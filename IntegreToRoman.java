b927ebe4a833a3e4fed25395fcd47c78b27f8932
Riccardo <123867129+DBX05@users.noreply.github.com>
2026-05-03 20:49:35 +0200

    Create IntegreToRoman.java

---
+++ IntegreToRoman.java
@@ -0,0 +1,24 @@
+package it.unipd.mtss;
+public class IntegerToRoman {
+
+    public static String convert(int number) {
+        if (number <= 0 || number > 1000) {
+            throw new IllegalArgumentException("Number must be between 1 and 1000");
+        }
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                result.append(symbols[i]);
+                number -= values[i];
+            }
+        }
+
+        return result.toString();
+    }
+}
+package it.unipd.mtss;
