2635b19b077eb7b9107485229f865a7295f3d632
pietro-brock <pietro.brocadello@studenti.unipd.it>
2026-05-07 10:35:04 +0200

    Aggiornato IntegerToRoman.java con header corretto

---
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////
+// PIETRO BROCADELLO 2147994
+// RICCARDO CONTE 2145596
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+
+    public static String convert(int number) {
+        if (number <= 0 || number > 1000) {
+            throw new IllegalArgumentException(
+                "Il numero deve essere compreso tra 1 e 1000"
+            );
+        }
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (number >= values[i]) {
+                result.append(symbols[i]);
+                number -= values[i];
+            }
+        }
+
+        return result.toString();
+    }
+}
